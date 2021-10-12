c50d5af1551ace6945b9901b56e19d83d8571233
XuYue280 <90425273+XuYue280@users.noreply.github.com>
2021-10-12 01:27:30 -0400

    Update common turm in two array.c

--- common turm in two array.c
+++ common turm in two array.c
@@ -12,7 +12,7 @@ int main()
 {
     int a[3]={1,2,7};
     int b[3]={2,4,6};
-    int total=6;
+    int total=6; //sum of two array length
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             if(a[i]==b[j]){
